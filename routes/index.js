72b43888b7e9d1111aaeb559ee693e4d723ae526
Shubham Deshmukh <35609231+shubham1295@users.noreply.github.com>
2019-06-26 21:19:28 +0530

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/shubham1295/SpendingTracker"""
    This reverts commit 356bb2603623f3eced584147a98adbc8ce4e7cc4.

--- routes/index.js
+++ routes/index.js
@@ -58,7 +58,8 @@ router.post("/", (req, res, next) => {
         cost: req.body.cost
     });
 
-    spend.date.setDate(spend.date.getDate());
+    // Format Selected Date by +1
+    spend.date.setDate(spend.date.getDate() +1);
     spend.save()
     .then(result => {
         // res.status(200).json({
