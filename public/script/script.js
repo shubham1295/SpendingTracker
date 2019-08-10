b199f5147a5fc0a07ce7bc09a13fe37eb22bb30b
Shubham Deshmukh <35609231+shubham1295@users.noreply.github.com>
2019-08-11 01:00:41 +0530

    Fixed #29 and code formatting

--- public/script/script.js
+++ public/script/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-  var date_input=$('input[id="date"]'); //our date input has the name "date"
-  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-  var options={
+$(document).ready(function () {
+  var date_input = $('input[id="date"]'); //our date input has the name "date"
+  var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+  var options = {
     format: 'dd/mm/yyyy',
     container: container,
     todayHighlight: true,
@@ -9,45 +9,50 @@ $(document).ready(function(){
     clearBtn: true,
     //defaultViewDate: { year: 1977, month: 04, day: 25 },  Change to Today's date
   };
-  date_input.datepicker(options); 
+  date_input.datepicker(options);
 
 });
 
-$(".myForm").submit(function(e) {
+$(".myForm").submit(function (e) {
 
-  e.preventDefault(); 
+  e.preventDefault();
 
   var form = $(this);
   var url = form.attr('action');
 
   $.ajax({
-         type: "POST",
-         url: url,
-         data: form.serialize(), 
-         success: function(data)
-         {
-            // $("#total-amount").text(parseInt($("#total-amount").text()) + data.result.cost);
+    type: "POST",
+    url: url,
+    data: form.serialize(),
+    success: function (data) {
+      // $("#total-amount").text(parseInt($("#total-amount").text()) + data.result.cost);
 
-            // $("#table-body").append('<tr><td>' + data.result.item + '</td><td>' + data.result.cost + '</td><td>' + data.result.date + '</td></tr>');
-            $.ajax({
-              type: "GET",
-              url: '/price',
-              dataType:"json",
-              success: function(data)
-              {
-                // console.log(data.totalExp);
-                $("#total-amount").text(data.totalExp);
+      // $("#table-body").append('<tr><td>' + data.result.item + '</td><td>' + data.result.cost + '</td><td>' + data.result.date + '</td></tr>');
+      $.ajax({
+        type: "GET",
+        url: '/price',
+        dataType: "json",
+        success: function (data) {
+          // console.log(data.totalExp);
+          $("#total-amount").text(data.totalExp);
 
-                $('.myForm')[0].reset();
-                // toast ka JS
-                var snackbarContainer = document.querySelector('#demo-toast-example');
-                var data = { message: 'Data Saved' };
-                snackbarContainer.MaterialSnackbar.showSnackbar(data);
-              }
-            });
-            
-         }
-       });
+          $('.myForm')[0].reset();
+          $(".table tbody").text("");
+          console.log(data.test);
+          Object.keys(data.test).forEach((key) => {
+            // console.log(data.test[key]._id);
+            $(".table").append('<tr><td>' + data.test[key].item + '</td><td>' + data.test[key].cost + '</td><td>' + data.test[key].date + '</td></tr>');
+          })
+
+          // toast ka JS
+          var snackbarContainer = document.querySelector('#demo-toast-example');
+          var data = { message: 'Data Saved' };
+          snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        }
+      });
+
+    }
+  });
 });
 
 
@@ -56,65 +61,65 @@ function autocomplete(inp, arr) {
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
   /*execute a function when someone writes in the text field:*/
-  inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
-      /*close any already open lists of autocompleted values*/
-      closeAllLists();
-      if (!val) { return false;}
-      currentFocus = -1;
-      /*create a DIV element that will contain the items (values):*/
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
-      this.parentNode.appendChild(a);
-      /*for each item in the array...*/
-      for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
-          b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
-              inp.value = this.getElementsByTagName("input")[0].value;
-              /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
-              closeAllLists();
-          });
-          a.appendChild(b);
-        }
+  inp.addEventListener("input", function (e) {
+    var a, b, i, val = this.value;
+    /*close any already open lists of autocompleted values*/
+    closeAllLists();
+    if (!val) { return false; }
+    currentFocus = -1;
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    /*append the DIV element as a child of the autocomplete container:*/
+    this.parentNode.appendChild(a);
+    /*for each item in the array...*/
+    for (i = 0; i < arr.length; i++) {
+      /*check if the item starts with the same letters as the text field value:*/
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        /*create a DIV element for each matching element:*/
+        b = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        /*insert a input field that will hold the current array item's value:*/
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        /*execute a function when someone clicks on the item value (DIV element):*/
+        b.addEventListener("click", function (e) {
+          /*insert the value for the autocomplete text field:*/
+          inp.value = this.getElementsByTagName("input")[0].value;
+          /*close the list of autocompleted values,
+          (or any other open lists of autocompleted values:*/
+          closeAllLists();
+        });
+        a.appendChild(b);
       }
+    }
   });
   /*execute a function presses a key on the keyboard:*/
-  inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
-        currentFocus++;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
-        currentFocus--;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
-        e.preventDefault();
-        if (currentFocus > -1) {
-          /*and simulate a click on the "active" item:*/
-          if (x) x[currentFocus].click();
-        }
+  inp.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      /*If the arrow DOWN key is pressed,
+      increase the currentFocus variable:*/
+      currentFocus++;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 38) { //up
+      /*If the arrow UP key is pressed,
+      decrease the currentFocus variable:*/
+      currentFocus--;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      /*If the ENTER key is pressed, prevent the form from being submitted,*/
+      e.preventDefault();
+      if (currentFocus > -1) {
+        /*and simulate a click on the "active" item:*/
+        if (x) x[currentFocus].click();
       }
+    }
   });
   function addActive(x) {
     /*a function to classify an item as "active":*/
@@ -144,19 +149,55 @@ function autocomplete(inp, arr) {
   }
   /*execute a function when someone clicks in the document:*/
   document.addEventListener("click", function (e) {
-      closeAllLists(e.target);
+    closeAllLists(e.target);
   });
 }
 
 $.ajax({
   type: "GET",
   url: '/search_item',
-  dataType:"json",
-  success: function(data)
-  {
+  dataType: "json",
+  success: function (data) {
     // console.log(data.message);
     var items = data.message;
 
     autocomplete(document.getElementById("item"), items);
   }
 });
+
+// -----------------  TEST  -------------------------
+
+// $(document).ready(function(){
+//   $('#demo-show-toast').on('click', function () {
+//     $.ajax({
+//       url: '/',
+//       type: 'POST',
+//       contentType: 'application/json',
+//       data: $("myForm").serialize(),
+//       success: function (response) {
+
+//         console.log(response);
+
+//         // var tbodyEl = $('tbody');
+//         // tbodyEl.html('');
+//         // response.result.forEach(function (result) {
+//         //   //   tbodyEl.append('\
+//         //   //   <tr>\
+//         //   //     <td class="item">' + result.item + ' </td>\
+//         //   //     <td class="item">' + result.cost + ' </td>\
+//         //   //     <td class="item">' + result.date + ' </td>\
+//         //   //   </tr>\
+//         //   // ');
+//         //   alert(resut);
+//         //});
+//       }
+//     });
+//   });
+// })
+
+// $('demo-show-toast').click(function(){
+//   $.post(window.location, $('.myform').serialize(),
+//   function(data, status){
+//     alert('Data: '+data+"\n status"+status);
+//   });
+// });
