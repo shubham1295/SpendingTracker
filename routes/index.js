cf0427454f0fb15a07775f5f0c01a0d2cf57784f
Shubham Deshmukh <35609231+shubham1295@users.noreply.github.com>
2019-08-11 01:00:57 +0530

    Code Formatting

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,25 @@ const Sugar = require('sugar');
 const Spending = require('../models/spending');
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+    "July", "August", "September", "October", "November", "December"
 ];
 
 
 router.get("/", (req, res, next) => {
 
     var currentDate = new Date();
-    var currMonth = currentDate.getMonth()+1;
+    var currMonth = currentDate.getMonth() + 1;
 
     // res.status(200).json({
     //     message: month,
     // });
 
-    GetQuery(Spending, function(result, test){
+    GetQuery(Spending, function (result, test) {
         // console.log(result,test);
         var totalExp = formatMoney(result[0].total);
-        
-        res.status(200).render('index', { total: totalExp, item: test, month: monthNames[currMonth], year: currentDate.getFullYear()});
-        
+
+        res.status(200).render('index', { total: totalExp, item: test, month: monthNames[currMonth], year: currentDate.getFullYear() });
+
 
     });
 
@@ -54,9 +54,9 @@ router.post("/", (req, res, next) => {
 
         // res.send(result);
     })
-    .catch(err => {
-        console.log(err);
-    });
+        .catch(err => {
+            console.log(err);
+        });
 
 
     // res.status(200).json({
@@ -141,26 +141,26 @@ router.post("/", (req, res, next) => {
 // });
 
 router.get("/price", (req, res, next) => {
-    GetQuery(Spending, function(result, test){
+    GetQuery(Spending, function (result, test) {
         // console.log(result,test);
         var totalExp = formatMoney(result[0].total);
         // res.status(200).render('test', { total: totalExp, item: test, month: monthNames[currMonth], year: currentDate.getFullYear()});
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ totalExp,test }, null, 3));
-        console.log(totalExp,test);
+        res.end(JSON.stringify({ totalExp, test }, null, 3));
+        console.log(totalExp, test);
     });
 });
 
-router.get('/search_item', (req,res,next) => {
+router.get('/search_item', (req, res, next) => {
     Spending.distinct('item')
-    .then(result => {
-        res.status(200).json({
-            message: result
+        .then(result => {
+            res.status(200).json({
+                message: result
+            });
+        })
+        .catch(err => {
+            console.log(err);
         });
-    })
-    .catch(err => {
-        console.log(err);
-    });
 });
 
 module.exports = router;
@@ -172,44 +172,47 @@ function GetFormattedDate(date) {
     var month = fields[1];
     var year = fields[2];
 
-    date= year+-+month+-+day;
+    date = year + -+month + -+day;
     return date;
 }
 
-function GetQuery(Spending,callback){
+function GetQuery(Spending, callback) {
     var currentDate = new Date();
-    var currMonth = currentDate.getMonth()+1;
+    var currMonth = currentDate.getMonth() + 1;
 
     Spending.aggregate([
         { $project: { month: { $month: '$date' }, cost: true } },
         { $match: { month: currMonth } },
-        { $group: {
-            _id: null,
-            total: { $sum: '$cost'}
+        {
+            $group: {
+                _id: null,
+                total: { $sum: '$cost' }
             }
         }
     ]).exec().then(result => {
-        if(result.length == 0){
+        if (result.length == 0) {
             //If result is null set total amount to 0
-            result = [{_id: null, total: 0}];
+            result = [{ _id: null, total: 0 }];
         }
 
         // console.log(Sugar.Date.format(new Date(), '%Y-%m-%d'));
         Spending.aggregate([
-            { $match: { date: {$gte: new Date(Sugar.Date.format(new Date(), '%Y-%m-%d')) } } },
-            { $group: {
+            { $match: { date: { $gte: new Date(Sugar.Date.format(new Date(), '%Y-%m-%d')) } } },
+            {
+                $group: {
                     _id: "$_id",
                     item: { $first: '$item' },
                     cost: { $first: '$cost' },
                     date: { $first: '$date' },
                 }
             },
-            { $project: { 
+            {
+                $project: {
                     _id: true,
-                    item: true, 
-                    cost: true, 
-                    date: { $dateToString: { format: "%d/%m/%G",date: "$date" } } 
-                } 
+                    item: true,
+                    cost: true,
+                    date: { $dateToString: { format: "%d/%m/%G", date: "$date" } }
+                }
             }
         ]).then(test => {
             // console.log(test,result);
@@ -220,33 +223,33 @@ function GetQuery(Spending,callback){
             // else {
             //     res.status(200).render('index', { total: result, item: test, month: monthNames[currMonth], year: currentDate.getFullYear()});
             // }
-            callback(result,test);
+            callback(result, test);
         }).catch(err => {
-                console.log(err);
-                // res.status(500).json(err);
-                callback(err);
+            console.log(err);
+            // res.status(500).json(err);
+            callback(err);
         });
-        
+
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 }
 
 
 function formatMoney(amount, decimalCount = 2, decimal = ".", thousands = ",") {
     try {
-      decimalCount = Math.abs(decimalCount);
-      decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
-  
-      const negativeSign = amount < 0 ? "-" : "";
-  
-      let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
-      let j = (i.length > 3) ? i.length % 3 : 0;
-  
-      return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+        decimalCount = Math.abs(decimalCount);
+        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+
+        const negativeSign = amount < 0 ? "-" : "";
+
+        let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+        let j = (i.length > 3) ? i.length % 3 : 0;
+
+        return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
     } catch (e) {
-      console.log(e)
+        console.log(e)
     }
-  };
+};
