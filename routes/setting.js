2d1bf937d57ab5ed55792e7d90e50c030baef261
Shubham Deshmukh <35609231+shubham1295@users.noreply.github.com>
2019-10-12 16:35:46 +0530

    Fixed deepscan issue

--- routes/setting.js
+++ routes/setting.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const Sugar = require('sugar');
-const Spending = require('../models/spending');
-const Category = require('../models/category');
 
 router.get("/", (req, res, next) => {
 
